981f4e3e6d9ab56d339c2234ea9670c2b2f63579
iopeti2u <hugo.iopeti5@etu.univ-lorraine.fr>
2021-05-03 16:49:18 +0200

    Dépôt du design pattern.

---
+++ src/carnet/designPattern/Observateur.java
@@ -0,0 +1,12 @@
+package carnet.designPattern;
+
+/**
+ * L'interface Observateur
+ */
+public interface Observateur {
+
+    /**
+     * Procédure qui fait réagir la vue qui l'implemente.
+     */
+    void reagir();
+}
